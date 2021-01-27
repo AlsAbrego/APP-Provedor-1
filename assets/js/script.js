d373a82bbebcfd480bac3364df00be0d4f718b8e
AlsAbrego <39961760+AlsAbrego@users.noreply.github.com>
2021-01-27 14:18:31 -0300

    project upload
    upload all project dependencies

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+// script para controlar o aparencia do menu Mobile quando esta ou não visível ou expandido.
+/*ESTARÁ DISPONÍVEL NA VERSÃO RESPONSIVA!!*/
+
+/*
+window.onload = function(){
+
+  document.querySelector(".menuMobile").addEventListener("click", function(){
+
+    if(document.querySelector(".menu nav ul").style.display == 'flex') {
+
+      document.querySelector(".menu nav ul").style.display = 'none';
+
+    } else {
+
+      document.querySelector(".menu nav ul").style.display = 'flex';
+
+    }
+  });
+
+};
+
+*/
